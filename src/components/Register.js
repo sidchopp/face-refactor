f4afd99b521ab8f753b423cc820c84e6e05fcd4d
Sid <sidchopp@gmail.com>
2021-09-24 11:30:34 -0600

    Back button added to Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,37 +3,24 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-//import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-// import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import HowToRegIcon from '@material-ui/icons/HowToReg';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-//import HowToRegIcon from '@mui/icons-material/HowToReg';
-
-
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const theme = createTheme();
 
-export default function Register({ onRouteChange, loadUser, setRoute }) {
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-  //   // eslint-disable-next-line no-console
-  //   console.log({
-  //     email: data.get('email'),
-  //     password: data.get('password'),
-  //   });
-  // };
+function Register({ onRouteChange, loadUser, setRoute }) {
 
   // States
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
+  // Event Handlers
   function onEmailChange(e) {
     e.preventDefault()
     setEmail(e.target.value)
@@ -131,24 +118,34 @@ export default function Register({ onRouteChange, loadUser, setRoute }) {
               // autoComplete="current-password"
               onChange={onPasswordChange}
             />
-
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              startIcon={<HowToRegIcon />}
               onClick={OnSubmitSignIn}
             >
               Register
             </Button>
+            <Button
+              type="submit"
+              fullWidth
+              color="secondary"
+              variant="outlined"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => setRoute('signIn')}
+              startIcon={<ArrowBackIosIcon />}
+            >
+              Back to Home
+            </Button>
             <Grid container>
-
-
             </Grid>
           </Box>
         </Box>
-
       </Container>
     </ThemeProvider >
   );
 }
+
+export default Register;
