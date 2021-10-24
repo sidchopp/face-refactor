dd9ad332b40a6d04a99fd4cf7f2da39e8019c9e7
Sid <sidchopp@gmail.com>
2021-10-23 20:46:57 -0600

    UseStyles for FaceRecog comp

--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
+
+// Components
+import UseStyles from './UseStyles';
 
 // CSS
 import '../App.css'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     height: '100vh',
+//   },
+//   paper: {
+//     margin: theme.spacing(8, 4),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+// }));
 
 function FaceRecognition({ imageURL, boundingBox }) {
-  const classes = useStyles();
+  const classes = UseStyles();
 
   return (
     <div className={classes.paper} >
